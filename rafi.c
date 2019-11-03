48e8271864709cac373a5dd2a71a5cc965276f4e
mustahsin al rafi <57066798+mustahsin15-2415@users.noreply.github.com>
2019-11-03 15:58:36 +0600

    Update rafi.c

--- rafi.c
+++ rafi.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,gun;
+    int a,b,gun,jog;
     scanf("%d%d",&a,&b);
     gun=a*b;
-    printf("%d",gun);
+    jog=a+b;
+    printf("gun=%d",gun);
+    printf("jog=%d",jog);
 }
